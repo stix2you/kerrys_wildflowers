00fd9ebe0fec3d0d09427ab6eaa722283833defe
Doug Hill <stix2you@gmail.com>
2024-07-03 16:07:54 -0500

    style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,31 @@ import styles from './Navbar.css';
 
 function Navbar() {
    return (
-      <nav className={styles.navbar}>
-         <div className={styles.logo}>
-            Wisconsin Wildflowers
+      <nav className="navbar">
+
+         <div className="header-upper">
+            <div className="logo">
+               WISCONSIN<br />WILDFLOWERS
+            </div>
+            <div className="search-and-cart menu center">
+            <Link to="/search" className="top_link no-underline" data-dropdown-rel="search">SEARCH</Link>
+            <Link to="/cart" className="top_link no-underline" title="Cart" data-no-instant="">
+              CART&nbsp;&nbsp;<span className="cart_count">(0)</span>
+            </Link>
+          </div>
          </div>
-         <ul className={styles.navLinks}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/education">Education</Link></li>
-            <li><Link to="/seeds">Seeds</Link></li>
-            <li><Link to="/canned-products">Canned Products</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-         </ul>
-         <div className={styles.hamburgerMenu}>
-            {/* Hamburger menu implementation */}
+
+         <div className="header-lower">
+            <ul className="navLinks">
+               <li><Link to="/">HOME</Link></li>
+               <li><Link to="/education">EDUCATION</Link></li>
+               <li><Link to="/seeds">SEEDS</Link></li>
+               <li><Link to="/canned-products">CANNED PRODUCTS</Link></li>
+               <li><Link to="/cart">CONTACT</Link></li>
+            </ul>
+            <div className={styles.hamburgerMenu}>
+               {/* Hamburger menu implementation */}
+            </div>
          </div>
       </nav>
    );
